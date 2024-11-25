40984910317f3201c0d8cc2916dfd253b3b9b558
sakhi baksh <sakhibux09@gmail.com>
2024-11-25 10:55:36 -0800

    my commit
    portfolio

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
 
+import React from "react";
+import logo from "./logo.svg"
 
+import "./App.css";
 
-import React, { useState } from "react";
-import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>MY PORTFOLIO</h1>
+        <p>Hello! I'm [Sakhi Baksh], a Web Developer.</p>
+      </header>
 
-const Clock = () => {
-    const curTime = new Date().toLocaleTimeString();
-    const [time, setTime] = useState(curTime);
+      <main>
+        <section className="about">
+          <h2>About Me</h2>
+          <p>I am learning Next js and building cool projects in Quarter 2 Governer it course.</p>
+        </section>
 
-    const update = () =>{
-      const curTime = new Date().toLocaleTimeString();
-      setTime(curTime)
-    }
-    setInterval(()=>{
-      update()
+        <section className="projects">
+          <h2>Projects</h2>
+          <ul>
+            <li>Project 1: Todo List App</li>
+            <li>Project 2: Responsive Website</li>
+            <li>Project 3: Simple Portfolio</li>
+          </ul>
+        </section>
 
+        <section className="contact">
+          <h2>Contact Me</h2>
+          <h3>Email: sakhibux09@gmail.com</h3>
+          <h4>Phone No: 03232679225</h4>
+        </section>
+      </main>
 
-    },1000
-  )
-    return (
-        <>
-        <div className="container">
-          <h1 className="text"><u>SAKHI</u></h1>
-          <h1 className="text">(<b>Digital Clock</b>)</h1>
-            <h1>{time}</h1>
-        </div>
-        </>
-    )
-
+      <footer>
+        <p>© 2024 My Portfolio. All Rights Reserved.</p>
+      </footer>
+    </div>
+  );
 }
-export default Clock
+
+export default App;
+
